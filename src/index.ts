68b8a9f2b4b1739a4e8715c145f1171dd5c065ce
tkgstrator <tkgstrator@users.noreply.github.com>
2025-03-05 17:12:19 +0000

    refactor(imports): simplify dayjs import statement

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { OpenAPIHono as Hono } from '@hono/zod-openapi'
 import { apiReference } from '@scalar/hono-api-reference'
-import * as dayjs from 'dayjs'
+import dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
